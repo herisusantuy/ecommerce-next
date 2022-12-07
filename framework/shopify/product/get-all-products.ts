4aa403a89b8466a2658cb751419e6c63e11bab74
askloraheri <heri.susanto@loratechai.com>
2022-12-08 05:23:55 +0700

    first commit

---
+++ framework/shopify/product/get-all-products.ts
@@ -0,0 +1,19 @@
+import getAllProductsQuery from '../utils/queries/get-all-products';
+import fetchApi from '../utils/fetch-api';
+import { ProductConnection } from '../schema';
+import { normalizeProduct } from '../utils/normalize';
+
+type ReturnType = {
+  products: ProductConnection;
+};
+
+const getAllProducts = async (): Promise<any> => {
+  const { data } = await fetchApi<ReturnType>({ query: getAllProductsQuery });
+
+  const products =
+    data.products.edges.map(({ node: product }) => normalizeProduct(product)) ||
+    [];
+  return products;
+};
+
+export default getAllProducts;
